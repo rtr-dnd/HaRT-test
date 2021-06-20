28e16d8aa8068b50b76b96b83d5dcb2c7313dc4c
CyberLab PC 9 <cyber@cyber.t.u-tokyo.ac.jp>
2021-06-20 23:40:32 +0900

    rotation interpolation testing

--- Assets/_Scripts/Redirection/RedirectionTechniques/InterpolationRedirection/InterpolationWithRotation.cs
+++ Assets/_Scripts/Redirection/RedirectionTechniques/InterpolationRedirection/InterpolationWithRotation.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
 
 namespace HR_Toolkit
 {
@@ -10,12 +12,70 @@ namespace HR_Toolkit
     /// Power
     /// </summary>
     public float p;
+    public float pRot;
 
 
     public override void ApplyRedirection(Transform realHandPos, Transform virtualHandPos, Transform warpOrigin, RedirectionObject target,
         Transform bodyTransform)
     {
       virtualHandPos.position = IDW(realHandPos.position, target);
+      virtualHandPos.rotation = IDWRot(realHandPos.position, realHandPos.rotation, target);
+    }
+
+    Quaternion AverageQuaternionWithWeight(List<Quaternion> qArray, List<float> wArray)
+    {
+      var wArrayNormalized = new List<float>();
+      foreach (var w in wArray)
+      {
+        wArrayNormalized.Add(w / wArray.Max());
+      }
+
+      float maxVal = float.MinValue;
+      int maxIndex = 0;
+      for (int i = 0; i < wArrayNormalized.Count; i++)
+      { // finding the largest w
+        if (wArrayNormalized[i] > maxVal)
+        {
+          maxVal = wArrayNormalized[i];
+          maxIndex = i;
+        }
+      }
+      Quaternion qAvg = qArray[maxIndex]; // initial qAvg is the one which wArray is the largest
+      qArray.RemoveAt(maxIndex);
+      wArrayNormalized.RemoveAt(maxIndex);
+
+      float standardWeight;
+      for (int i = 1; i < qArray.Count; i++)
+      {
+        standardWeight = 1.0f / (float)(i + 1);
+        qAvg = Quaternion.Slerp(qAvg, qArray[i], standardWeight * wArrayNormalized[i]);
+      }
+      return qAvg;
+    }
+
+    private Quaternion IDWRot(Vector3 x, Quaternion xRot, RedirectionObject target)
+    {
+      var points = target.GetAllPositions();
+
+      var qArray = new List<Quaternion>();
+      var wArray = new List<float>();
+
+      qArray.Add(xRot);
+      wArray.Add(1); // todo: determine threshold
+
+      foreach (var point in points)
+      {
+        var d = Vector3.Distance(x, point.GetRealPosition());
+        if (d == 0f) // todo: determine threshold
+        {
+          return point.GetVirtualRotation();
+        }
+        var w = Mathf.Pow(1 / d, pRot);
+        qArray.Add(point.GetVirtualRotation());
+        wArray.Add(w);
+      }
+
+      return AverageQuaternionWithWeight(qArray, wArray);
     }
 
     private Vector3 IDW(Vector3 x, RedirectionObject target)
--- Assets/_Scripts/Redirection/VirtualToRealConnection.cs
+++ Assets/_Scripts/Redirection/VirtualToRealConnection.cs
@@ -3,98 +3,108 @@ using UnityEngine;
 
 namespace HR_Toolkit.Redirection
 {
-    [ExecuteAlways]
-    public class VirtualToRealConnection : MonoBehaviour
+  [ExecuteAlways]
+  public class VirtualToRealConnection : MonoBehaviour
+  {
+    public Transform realPosition;
+    public Transform virtualPosition;
+
+    public Material defaultConnectionMaterial;
+    public Material highlightedConnectionMaterial;
+
+    private LineRenderer _lineRenderer;
+
+    private void Start()
     {
-        public Transform realPosition;
-        public Transform virtualPosition;
-        
-        public Material defaultConnectionMaterial;
-        public Material highlightedConnectionMaterial;
+      if (GetComponent<LineRenderer>() == null)
+      {
+        _lineRenderer = gameObject.AddComponent<LineRenderer>();
+      }
 
-        private LineRenderer _lineRenderer;
+      if (_lineRenderer == null)
+      {
+        _lineRenderer = GetComponent<LineRenderer>();
+      }
 
-        private void Start()
-        {
-            if (GetComponent<LineRenderer>() == null)
-            {
-                _lineRenderer = gameObject.AddComponent<LineRenderer>();
-            }
+      _lineRenderer.startWidth = 0.005f;
+      _lineRenderer.endWidth = 0.005f;
 
-            if (_lineRenderer == null)
-            {
-                _lineRenderer = GetComponent<LineRenderer>();
-            }
-            
-            _lineRenderer.startWidth = 0.005f;
-            _lineRenderer.endWidth = 0.005f;
+      _lineRenderer.startColor = Color.green;
+      _lineRenderer.endColor = Color.green;
+      _lineRenderer.material = defaultConnectionMaterial;
 
-            _lineRenderer.startColor = Color.green;
-            _lineRenderer.endColor = Color.green;
-            _lineRenderer.material = defaultConnectionMaterial;
-            
-            gameObject.layer = LayerMask.NameToLayer("Visualization");
-            
-            ChangeMaterialToDefault();
-        }
+      gameObject.layer = LayerMask.NameToLayer("Visualization");
 
-        private void Update()
-        {
-            _lineRenderer.SetPosition(0, realPosition.position);
-            _lineRenderer.SetPosition(1, virtualPosition.position);
-        }
-
-        public void ChangeMaterialToDefault()
-        {
-            _lineRenderer.material = defaultConnectionMaterial;
-        }
-
-        public void ChangeMaterialToHighlighted()
-        {
-            try
-            {
-                _lineRenderer.material = highlightedConnectionMaterial;
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-        }
-
-        public Vector3 GetRealPosition()
-        {
-            return realPosition.position;
-        }
-
-        public Vector3 GetVirtualPosition()
-        {
-            return virtualPosition.position;
-        }
-
-        void OnDrawGizmos()
-        {
-            if (realPosition == null) return;
-
-            if (GetRealPosition() == GetVirtualPosition())
-            {
-                Gizmos.color = Color.cyan;
-                Gizmos.DrawSphere(GetRealPosition(), .007f);
-            }
-            else
-            {
-                // real positions
-                Gizmos.color = Color.yellow;
-                Gizmos.DrawSphere(GetRealPosition(), .007f);
-                
-                // virtual positions
-                Gizmos.color = Color.blue;
-                Gizmos.DrawSphere(GetVirtualPosition(), .007f);
-                
-                Gizmos.color = Color.red;
-                Gizmos.DrawLine(GetRealPosition(), GetVirtualPosition());
-            }
-        }
+      ChangeMaterialToDefault();
     }
+
+    private void Update()
+    {
+      _lineRenderer.SetPosition(0, realPosition.position);
+      _lineRenderer.SetPosition(1, virtualPosition.position);
+    }
+
+    public void ChangeMaterialToDefault()
+    {
+      _lineRenderer.material = defaultConnectionMaterial;
+    }
+
+    public void ChangeMaterialToHighlighted()
+    {
+      try
+      {
+        _lineRenderer.material = highlightedConnectionMaterial;
+
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e);
+        throw;
+      }
+    }
+
+    public Vector3 GetRealPosition()
+    {
+      return realPosition.position;
+    }
+
+    public Vector3 GetVirtualPosition()
+    {
+      return virtualPosition.position;
+    }
+
+    public Quaternion GetRealRotation()
+    {
+      return realPosition.rotation;
+    }
+
+    public Quaternion GetVirtualRotation()
+    {
+      return virtualPosition.rotation;
+    }
+
+    void OnDrawGizmos()
+    {
+      if (realPosition == null) return;
+
+      if (GetRealPosition() == GetVirtualPosition())
+      {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(GetRealPosition(), .007f);
+      }
+      else
+      {
+        // real positions
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(GetRealPosition(), .007f);
+
+        // virtual positions
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(GetVirtualPosition(), .007f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(GetRealPosition(), GetVirtualPosition());
+      }
+    }
+  }
 }
